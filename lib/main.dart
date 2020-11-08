05cb6bf6571fe951d00f6e72ef2539485a30cdd5
tachyon <abdullai.tahiru@gmail.com>
2020-11-08 15:12:21 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as Path;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ImageUpload(),
+    );
+  }
+}
+
+class ImageUpload extends StatefulWidget {
+  ImageUpload({Key key}) : super(key: key);
+
+  @override
+  _ImageUploadState createState() => _ImageUploadState();
+}
+
+class _ImageUploadState extends State<ImageUpload> {
+  File _image;
+  final picker = ImagePicker();
+  String _uploadedFileURL;
+
+  Future pick_and_upload_image() async {
+    final pickedImage = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedImage != null) {
+        _image = File(pickedImage.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future<String> uploadFile() async {
+    try {
+      var taskSnapshot = await firebase_storage.FirebaseStorage.instance
+          .ref('uploads/${Path.basename(_image.path)}')
+          .putFile(_image);
+      taskSnapshot.ref.getDownloadURL().then((fileURL) {
+        print(fileURL);
+        setState(() {
+          _uploadedFileURL = fileURL;
+        });
+      });
+    } on firebase_storage.FirebaseStorage catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firestore File Upload'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Selected Image'),
+            _image != null
+                ? Image.asset(
+                    _image.path,
+                    height: 150,
+                  )
+                : Container(height: 150),
+            _image == null
+                ? RaisedButton(
+                    child: Text('Choose File'),
+                    onPressed: pick_and_upload_image,
+                    color: Colors.cyan,
+                  )
+                : Container(),
+            _image != null
+                ? RaisedButton(
+                    child: Text('Upload File'),
+                    onPressed: uploadFile,
+                    color: Colors.cyan,
+                  )
+                : Container(),
+            _image != null
+                ? RaisedButton(
+                    child: Text('Clear Selection'),
+                    onPressed: () {
+                      setState(() {
+                        _image = null;
+                      });
+                    })
+                : Container(),
+            Text('Uploaded Image'),
+            _uploadedFileURL != null
+                ? Image.network(
+                    _uploadedFileURL,
+                    height: 150,
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
